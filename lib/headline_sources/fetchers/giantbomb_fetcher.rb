641734b7a255f98a8bea91f590602acb3b1f58bd
Wil Gieseler <wil@wilgieseler.com>
2013-12-31 12:12:02 -0800

    update giantbomb

--- lib/headline_sources/fetchers/giantbomb_fetcher.rb
+++ lib/headline_sources/fetchers/giantbomb_fetcher.rb
@@ -1,13 +1,23 @@
 require "headline_sources/scraper"
 
 module HeadlineSources
-  class GiantBombFetcher < Scraper
+  class GiantbombFetcher < Scraper
 
     def scrape_page(i)
       doc = Nokogiri::HTML(open("http://www.giantbomb.com/news/?page=#{i}"))
       doc.css('.editorial.river h3').each do |link|
         add_headline! link.content
       end
+    end
+
+    def excluded_matches
+      [
+        "Ryan Davis, 1979 - 2013",
+        "Top 10",
+        "s Top",
+        "Game of the Year Awards",
+        "Giant Bomb's"
+      ]
     end
 
   end
