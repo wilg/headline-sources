bfb05899c30c3b6ae5c22d1e0dab4c64bfd78bea
Wil Gieseler <wil@wilgieseler.com>
2014-10-26 14:28:48 -0700

    fix hitfix, add rss

--- lib/headline_sources/fetchers/hitfix_fetcher.rb
+++ lib/headline_sources/fetchers/hitfix_fetcher.rb
@@ -1,14 +1,23 @@
 require "headline_sources/scraper"
 
 module HeadlineSources
-  class HitfixFetcher < Scraper
+
+  class HitfixFetcher < RSSFetcher
+
+    def feed_url
+      "http://www.hitfix.com/headlines.rss"
+    end
+
+  end
+
+  class HitfixFetcher2 < Scraper
 
     def url_for_progress(n)
       "http://www.hitfix.com/archive/all/?id=all&page=#{n}"
     end
 
     def headline_css_selector
-      '.article-head h2 a'
+      '.listing .details h2'
     end
 
   end
