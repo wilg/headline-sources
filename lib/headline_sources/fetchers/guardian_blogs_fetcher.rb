de66ec279209b6b8ddc33d3fb873c47c67bbb812
Wil Gieseler <wil@wilgieseler.com>
2014-10-26 14:59:06 -0700

    the guardian full

--- lib/headline_sources/fetchers/guardian_blogs_fetcher.rb
+++ lib/headline_sources/fetchers/guardian_blogs_fetcher.rb
@@ -1,7 +1,14 @@
 require "headline_sources/scraper"
 
 module HeadlineSources
-  class GuardianBlogsFetcher < Scraper
+
+  class GuardianBlogsFetcher < RSSFetcher
+    def feed_url
+      ["http://www.theguardian.com/uk/rss", "http://www.theguardian.com/us/rss"]
+    end
+  end
+
+  class GuardianBlogsFetcher2 < Scraper
 
     def url_for_progress(i)
       "http://www.theguardian.com/news/blog?page=#{i}"
