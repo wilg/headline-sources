b5888343e0b0a4214de3c0baa8f7e646a4f6a808
Wil Gieseler <wil@wilgieseler.com>
2014-10-26 14:43:50 -0700

    fix bro bible, add rss

--- lib/headline_sources/fetchers/bro_bible_fetcher.rb
+++ lib/headline_sources/fetchers/bro_bible_fetcher.rb
@@ -1,15 +1,23 @@
 require "headline_sources/scraper"
 
 module HeadlineSources
-  class BroBibleFetcher < Scraper
+
+  class BroBibleFetcher < RSSFetcher
+    def feed_url
+      "http://feeds.feedburner.com/BroBible"
+    end
+  end
+
+  class BroBibleFetcher2 < Scraper
 
     def url_for_progress(n)
       "http://www.brobible.com/page/#{n}/"
     end
 
     def headline_css_selector
-      '.post-title h2 a'
+      '.block h1 a'
     end
 
   end
+
 end
