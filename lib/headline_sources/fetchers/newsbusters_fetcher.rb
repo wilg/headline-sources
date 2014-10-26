52e16d0b25688c816c4a9755186d92b54d123df5
Wil Gieseler <wil@wilgieseler.com>
2014-10-26 14:47:30 -0700

    fix newsbusters, rss

--- lib/headline_sources/fetchers/newsbusters_fetcher.rb
+++ lib/headline_sources/fetchers/newsbusters_fetcher.rb
@@ -1,15 +1,23 @@
 require "headline_sources/scraper"
 
 module HeadlineSources
-  class NewsbustersFetcher < Scraper
+
+  class NewsbustersFetcher < RSSFetcher
+    def feed_url
+      "http://newsbusters.org/blog/feed"
+    end
+  end
+
+  class NewsbustersFetcher2 < Scraper
 
     def url_for_progress(n)
-      "http://newsbusters.org/archive?page=#{n}"
+      "http://newsbusters.org/more-posts?page=#{n}"
     end
 
     def headline_css_selector
-      '.field-content h2.title'
+      '.views-row .views-field-title a'
     end
 
   end
+
 end
