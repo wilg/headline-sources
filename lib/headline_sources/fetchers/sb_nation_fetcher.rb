0ce45ff971c56d94848740b3c052c06a7097d62a
Wil Gieseler <wil@wilgieseler.com>
2014-10-26 16:45:08 -0700

    sb nation rss

--- lib/headline_sources/fetchers/sb_nation_fetcher.rb
+++ lib/headline_sources/fetchers/sb_nation_fetcher.rb
@@ -1,7 +1,12 @@
 require "headline_sources/scraper"
 
 module HeadlineSources
-  class SbNationFetcher < Scraper
+  class SbNationFetcher < RSSFetcher
+    def feed_url
+      "http://feeds.feedburner.com/sportsblogs/sbnation.xml"
+    end
+  end
+  class SbNationFetcher2 < Scraper
 
     def url_for_progress(n)
       "http://www.sbnation.com/latest-news/archives/#{n}/"
