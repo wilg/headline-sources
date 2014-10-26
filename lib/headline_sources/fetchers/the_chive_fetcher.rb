9f91d390087e0c2eaa970fd31ade832f87c67ddd
Wil Gieseler <wil@wilgieseler.com>
2014-10-26 14:24:11 -0700

    fix the chive, add rss

--- lib/headline_sources/fetchers/the_chive_fetcher.rb
+++ lib/headline_sources/fetchers/the_chive_fetcher.rb
@@ -1,15 +1,8 @@
 require "headline_sources/scraper"
 
 module HeadlineSources
-  class TheChiveFetcher < Scraper
 
-    def url_for_progress(i)
-      "http://thechive.com/page/#{i}/"
-    end
-
-    def headline_css_selector
-      'h2.post-title a'
-    end
+  module ChiveExclusions
 
     def format_headline(headline)
       super(headline).gsub(/\((.+)\)/, '').gsub("….", "")
@@ -25,4 +18,29 @@ module HeadlineSources
     end
 
   end
+
+  class TheChiveFetcher < RSSFetcher
+
+    include ChiveExclusions
+
+    def feed_url
+      "http://thechive.com/feed/"
+    end
+
+  end
+
+  class TheChiveFetcher2 < Scraper
+
+    include ChiveExclusions
+
+    def url_for_progress(i)
+      "http://thechive.com/page/#{i}/"
+    end
+
+    def headline_css_selector
+      'h1.post-title a'
+    end
+
+  end
+
 end
