c8b2e53be9524cca16128cd24ddd82dbed16e0fe
Wil Gieseler <wil@wilgieseler.com>
2015-10-21 18:52:23 -0700

    fix broken scrapers

--- lib/headline_sources/fetchers/distractify_fetcher.rb
+++ lib/headline_sources/fetchers/distractify_fetcher.rb
@@ -4,11 +4,23 @@ module HeadlineSources
   class DistractifyFetcher < Scraper
 
     def url_for_progress(n)
-      "http://news.distractify.com/site/list/skip/#{(n - 1) * 100}/limit/100"
+      [
+        "http://distractify.com/",
+        "http://distractify.com/trending",
+        "http://distractify.com/entertainment",
+        "http://distractify.com/fyi",
+        "http://distractify.com/sex-relationships",
+        "http://distractify.com/food",
+        "http://distractify.com/videos",
+      ][n - 1]
     end
 
     def headline_css_selector
-      '.post-item h3 a'
+      '.index-feed__title'
+    end
+
+    def headline_href_prefix
+      "http://distractify.com"
     end
 
   end
