131a9ff887366e9f4f997f38894617211cc6f89b
Wil Gieseler <wil@wilgieseler.com>
2020-03-13 18:15:14 -0700

    gossip

--- lib/headline_sources/scraper.rb
+++ lib/headline_sources/scraper.rb
@@ -30,7 +30,7 @@ module HeadlineSources
     end
 
     def http_headers
-      {}
+      {'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'}
     end
 
     # Override this or override url_for_progress and headline_css_selector
